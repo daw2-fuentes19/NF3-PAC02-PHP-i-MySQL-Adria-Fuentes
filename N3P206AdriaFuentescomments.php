c38f6ac1a145f069cbcb562056d908a5d723a2e2
daw2-fuentes19 <43651378+daw2-fuentes19@users.noreply.github.com>
2019-10-22 15:00:06 +0200

    Add files via upload

---
+++ N3P206AdriaFuentescomments.php
@@ -0,0 +1,21 @@
+<?php
+
+echo "List of the 10 most important concepts";
+echo "<br/>";
+echo "<li>query 2 tables</li>";
+echo "<li>extract row</li>";
+echo "<li>while row</li>";
+echo "<li>mysqli_connect</li>";
+echo "<li>mysql relationship query</li>";
+echo "<li>comprobar resultado</li>";
+echo "<li>LINK</li>";
+echo "<li>INSERT</li>";
+echo "<li>mysqli error</li>";
+echo "<li>mysqli connect database</li>";
+echo "<br/>";
+echo "NOTA DEL DOCUMENTO: 8";
+echo "<br/>";
+echo "NOTA ESTUDIANTE: 9";
+echo "<br/>";
+echo "EXPLICA UNA MEJORA: No veo nada a cambiar.";
+?>
